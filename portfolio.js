fb9417062b5a049cee5216cbfbef3a1f5457ac74
shreyadhingra12 <shreyadhingra31@gmail.com>
2025-08-19 09:44:49 +0530

    let me know more
    html css and js file

---
+++ portfolio.js
@@ -0,0 +1,2 @@
+const typed = new Typed(".text", { strings: ['Software Developer', 'Frontend Developer','UI/UX Designer'], 
+                            typeSpeed: 100, backSpeed: 100, backDelay: 1000, loop: true });
